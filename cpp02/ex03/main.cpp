ac7e5ff1a3f33bfe3f155b10b9ec50c8713e9d7a
Jonni Le <jole@student.hive.fi>
2023-08-25 16:26:52 +0300

    did ex03

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -3,10 +3,10 @@
 bool bsp(Point const a, Point const b, Point const c, Point const point);
 
 int main () {
-	const Point a(0,0);
-	const Point b(10,10);
-	const Point c(10, 0);
-	const Point point(6,6);
+	const Point a(0, 0);
+	const Point b(10, 30);
+	const Point c(20, 0);
+	const Point point(19, 1);
 
 	std::cout << bsp(a, b, c, point) << std::endl;
 	return (0);
