55920e2553ac41652741d419ecca92668635fd15
Jonni Le <jole@student.hive.fi>
2023-10-03 16:01:43 +0300

    started 0403

---
+++ cpp04/ex03/Character.hpp
@@ -0,0 +1,16 @@
+#ifndef CHARACTER_HPP
+# define CHARACTER_HPP
+
+#include "AMateria.hpp"
+#include <iostream>
+
+class Character {
+	public:
+		virtual ~ICharacter() {}
+		virtual std::string const & getName() const = 0;
+		virtual void equip(AMateria* m) = 0;
+		virtual void unequip(int idx) = 0;
+		virtual void use(int idx, ICharacter& target) = 0;
+};
+
+#endif
