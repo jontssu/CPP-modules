57cea33d20b4fba5b28fdefe91de62125f51de3a
Jonni Le <jole@student.hive.fi>
2023-10-23 11:32:22 +0300

    fixed 0701

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -2,6 +2,11 @@
 # define ITER_HPP
 
 template<typename T>
+void f3(T& elem) {
+	elem += 1;
+}
+
+template<typename T>
 void iter(T* arr, int len, void (*f)(T& elem)) {
 	for (int i = 0; i < len; i++) {
 		f(arr[i]);
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -16,8 +16,16 @@ int main()
 	iter(j, 3, f); 
 	std::cout << j[0] << '\n' << j[1]  << '\n' << j[2] << '\n';
 
+	std::cout << "\n\n";
+
 	int x[] = {1, 2, 3, 4};
 	iter(x, 4, f2);
 	std::cout << x[0] << '\n' << x[1]  << '\n' << x[2] << '\n' << x[3] << '\n';
+
+	std::cout << "\n\n";
+
+	int y[] = {55, 66, 77, 88};
+	iter(y, 4, f3);
+	std::cout << y[0] << '\n' << y[1]  << '\n' << y[2] << '\n' << y[3] << '\n';
 	return 0;
 }
