fe7864eecd30e6ef81992b2ba57d89acefe1dd1e
Jonni Le <jole@student.hive.fi>
2024-04-11 17:40:09 +0300

    progress

--- cpp09/ex02/PmergeME.cpp
+++ cpp09/ex02/PmergeME.cpp
@@ -15,5 +15,28 @@ PmergeME::PmergeME(char **argv) {
 }
 
 void PmergeME::executeVector(int stride) {
-	
+	std::list<int> main;
+	std::list<int> pend;
+	int i = 0;
+	int distance = 0;
+	int tmp;
+	while ((int)vec.size() - distance >= stride * 2) {
+	std::cout << "pair: " << vec[i] << " & " << vec[i + stride] << '\n'; 
+		if (vec[i] < vec[i + stride]) {
+			tmp = vec[i];
+			vec[i] = vec[i + stride];
+			vec[i + stride] = tmp;
+		}
+		distance += stride * 2;
+		i += stride * 2;
+	}
+	std::cout << '\n';
+	for (auto x : vec) {
+		std::cout << x << ' ';
+	}
+	std::cout << '\n';
+	std::cout << '\n';
+	if ((int)vec.size() >= stride * 4) {
+		executeVector(stride * 2);
+	}
 }
--- cpp09/ex02/PmergeME.hpp
+++ cpp09/ex02/PmergeME.hpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <deque>
 #include <sstream>
+#include <list>
 
 class PmergeME {
 	public:
